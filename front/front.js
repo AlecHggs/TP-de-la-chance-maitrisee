5fd5d4b18a28984830433967402a9a1a621fd200
AlecHggs <alec.m.higgins@gmail.com>
2024-10-01 15:57:21 +0200

    add chat window

--- front/front.js
+++ front/front.js
@@ -2,21 +2,36 @@ const socket = io('http://localhost:3000')
 socket.on('connect', () => {
   console.log('connected')
 })
+var loginModal = new bootstrap.Modal(document.getElementById('login'));
+
+window.addEventListener('load', function() {
+    loginModal.show();
+});
 
 function submitUsername(){
     const input = document.getElementById('UsenameInput');
-    const login = document.getElementById('login');
-
     const username = input.value;
     if(username !== ""){
         socket.emit("username", username);
         input.value = '';
-        login.classList.add("invisible")
+        loginModal.hide()
     } 
+}
+
+function sendMessage(){
+    const chatinput = document.getElementById('ChatInput');
+    const message = chatinput.value;
+    if(message !== ""){
+        socket.emit("chat message", message)
+    }
 }
 
 socket.on('welcome', (welcomeMessage)=>{
     console.log({welcomeMessage})
     document.getElementById('welcomeMessage').innerHTML = welcomeMessage;
-
 })
+
+
+//<div class="chat-message sent-message">
+//    Hello! How can I help you?
+//</div>
