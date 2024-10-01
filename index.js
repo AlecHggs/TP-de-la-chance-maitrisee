23e29ee16addbec243ac7bfb29eb8b492db548d6
Mathis Baveye <mathisbaveye@MacBook-Air-de-Mathis.local>
2024-10-01 16:17:00 +0200

    Game full management

--- index.js
+++ index.js
@@ -14,32 +14,39 @@ const io = new Server(server,
 
 let users = []
 let socketUser = {}
+let roomId = "UniqueId"
 
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id)
 
   socket.on('username', (username) => {
-    console.log('Username received:', username);
-    users.push(username)
-    socketUser[socket.id] = username
-    const welcomeMessage = `Bienvenue ${username}, nous sommes ${users.length}`;
-    socket.emit('welcome', welcomeMessage);
-    const joinGameMessage = `${socketUser[socket.id]} a quitté.`;
-    socket.broadcast.emit('toast', 'success', joinGameMessage);
-    if(users.length > 2){
-      io.emit('game start');
+    if(users.length > 3){
+      const welcomeMessage = `Bonjour ${username}, nous sommes ${users.length}. Nous ne pouvons pas vous ajouter à la partie`;
+      socket.emit('welcome', welcomeMessage);
+    }else{
+      socket.join(roomId);
+      console.log('Username received:', username);
+      users.push(username)
+      socketUser[socket.id] = username
+      const welcomeMessage = `Bienvenue ${username}, nous sommes ${users.length}`;
+      socket.emit('welcome', welcomeMessage);
+      const joinGameMessage = `${socketUser[socket.id]} a quitté.`;
+      socket.to(roomId).emit('toast', 'success', joinGameMessage);
+      if(users.length > 2){
+        io.to(roomId).emit('game start');
+      }
     }
   });
 
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg); 
-    socket.broadcast.emit('chat message', socketUser[socket.id], msg);
+    socket.to(roomId).emit('chat message', socketUser[socket.id], msg);
   });
 
   socket.on('disconnect', () => {
     console.log('user disconnected', socket.id)
     const leftGameMessage = `${socketUser[socket.id]} a quitté.`;
-    socket.broadcast.emit('toast', 'danger', leftGameMessage);
+    socket.to(roomId).emit('toast', 'success', leftGameMessage);
     delete socketUser[socket.id];
   })
 })
